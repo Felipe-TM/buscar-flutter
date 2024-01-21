1b20336c1decaf3518937dc47fb40ec26e6b175e
Felipe Torres <92233132+Felipe-TM@users.noreply.github.com>
2024-01-21 16:39:25 -0300

    appointments page update

--- lib/Widgets/appointments_page.dart
+++ lib/Widgets/appointments_page.dart
@@ -1,3 +1,4 @@
+import 'package:buscar/Widgets/Login/shrinkeble_button.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentsPage extends StatelessWidget {
@@ -18,14 +19,41 @@ class AppointmentsPage extends StatelessWidget {
                 padding: EdgeInsets.only(top: 90),
                 child: Column(
                   children: [
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
-                    AppointmentTile(),
+                    AppointmentTile(
+                      destination: 'R. João Ruíz, 290',
+                      numberOfPassengers: '2',
+                      departureTime: '14:25',
+                      status: 'Pendente',
+                      date: '22/01/2024',
+                    ),
+                    AppointmentTile(
+                      destination: 'Av. Juscelino Kubitscheck, 3080',
+                      numberOfPassengers: '2',
+                      departureTime: '14:25',
+                      status: 'Pendente',
+                      date: '22/01/2024',
+                    ),
+                    AppointmentTile(
+                      destination: 'R. Geraldo Francisco dos Santos',
+                      numberOfPassengers: '2',
+                      departureTime: '14:25',
+                      status: 'Pendente',
+                      date: '22/01/2024',
+                    ),
+                    AppointmentTile(
+                      destination: 'R. João Ruíz, 290',
+                      numberOfPassengers: '2',
+                      departureTime: '14:25',
+                      status: 'Pendente',
+                      date: '22/01/2024',
+                    ),
+                    AppointmentTile(
+                      destination: 'R. João Ruíz, 290',
+                      numberOfPassengers: '2',
+                      departureTime: '14:25',
+                      status: 'Pendente',
+                      date: '22/01/2024',
+                    ),
                   ],
                 ),
               ),
@@ -39,55 +67,89 @@ class AppointmentsPage extends StatelessWidget {
 }
 
 class AppointmentTile extends StatelessWidget {
+  final String destination;
+  final String numberOfPassengers;
+  final String departureTime;
+  final String status;
+  final String date;
+
   const AppointmentTile({
     super.key,
+    required this.destination,
+    required this.numberOfPassengers,
+    required this.departureTime,
+    required this.status,
+    required this.date,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: SizedBox(
-        height: 150,
-        width: 350,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            border: Border.all(
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 5,
-                offset: Offset(5, 5),
+    return ShrinkebleButton(
+      shrinkScale: 0.95,
+      onPressed: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          height: 150,
+          width: 350,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.inversePrimary,
               ),
-            ],
-          ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text('Para:'), Text('12/01/2024')],
-                  ),
-                  Text('Rua Olando Silva, n 845'),
-                  Row(
-                    children: [
-                      Text('Saída:'),
-                      Padding(
-                        padding: EdgeInsets.only(left: 10),
-                        child: Text('17:55'),
-                      )
-                    ],
-                  ),
-                  Text('Status:'),
-                  Text('Em andamento')
-                ]),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 5,
+                  offset: Offset(5, 5),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Para:'),
+                        Expanded(
+                            child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            destination,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        )),
+                        Text(date)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Passageiros:'),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(numberOfPassengers),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Saída:'),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(departureTime),
+                        )
+                      ],
+                    ),
+                    const Text('Status:'),
+                    Text(status)
+                  ]),
+            ),
           ),
         ),
       ),
@@ -124,6 +186,7 @@ class ListFilters extends StatelessWidget {
               color: Theme.of(context).colorScheme.inversePrimary,
               child: const DropdownMenu<String>(
                   initialSelection: 'Hoje',
+                  width: 120,
                   enableSearch: false,
                   dropdownMenuEntries: [
                     DropdownMenuEntry(
@@ -144,6 +207,7 @@ class ListFilters extends StatelessWidget {
             Container(
               color: Theme.of(context).colorScheme.inversePrimary,
               child: const DropdownMenu(
+                  width: 150,
                   initialSelection: 'Passageiro',
                   enableSearch: false,
                   dropdownMenuEntries: [
