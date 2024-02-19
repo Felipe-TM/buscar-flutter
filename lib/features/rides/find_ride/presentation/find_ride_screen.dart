6e8a4109eeffd900f5e406651a06d5c852d20834
Felipe Torres <92233132+Felipe-TM@users.noreply.github.com>
2024-02-19 17:07:58 -0300

    Google Maps Addition

--- lib/features/rides/find_ride/presentation/find_ride_screen.dart
+++ lib/features/rides/find_ride/presentation/find_ride_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:buscar/common_widgets/flat_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class FindRideScreen extends StatelessWidget {
   const FindRideScreen({super.key});
@@ -10,6 +14,81 @@ class FindRideScreen extends StatelessWidget {
         title: const Text('Procurando Carona'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      body: const GoogleMapWidget(),
+      bottomNavigationBar: RideBottomNavBar(),
+    );
+  }
+}
+
+class RideBottomNavBar extends StatefulWidget {
+  const RideBottomNavBar({
+    super.key,
+  });
+
+  @override
+  State<RideBottomNavBar> createState() => _RideBottomNavBarState();
+}
+
+class _RideBottomNavBarState extends State<RideBottomNavBar> {
+  late int currentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    currentPage = 1;
+
+    return SizedBox(
+      height: 80,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          FlatIconButton(
+            onPressed: () {
+              currentPage = 1;
+            },
+            label: const Text('1'),
+            icon: const Icon(Icons.map_outlined),
+          ),
+          FlatIconButton(
+            onPressed: () {
+              currentPage = 2;
+            },
+            label: const Text('2'),
+            icon: const Icon(Icons.list_alt),
+          ),
+          FlatIconButton(
+              onPressed: () {
+                currentPage = 3;
+              },
+              label: const Text('3'),
+              icon: const Icon(Icons.check_sharp))
+        ].map((widget) => Expanded(child: widget)).toList(),
+      ),
+    );
+  }
+}
+
+class GoogleMapWidget extends StatefulWidget {
+  const GoogleMapWidget({super.key});
+
+  @override
+  State<GoogleMapWidget> createState() => _GoogleMapWidgetState();
+}
+
+class _GoogleMapWidgetState extends State<GoogleMapWidget> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition initialPos = CameraPosition(
+      target: LatLng(-23.316321971739562, -51.17272870369577),
+      zoom: 19.151926040649414);
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: initialPos,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
