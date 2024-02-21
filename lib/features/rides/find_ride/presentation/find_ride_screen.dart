6eccf71d866988214f5e9629849061c1910af33b
Felipe Torres <92233132+Felipe-TM@users.noreply.github.com>
2024-02-21 17:02:10 -0300

    google maps themes

--- lib/features/rides/find_ride/presentation/find_ride_screen.dart
+++ lib/features/rides/find_ride/presentation/find_ride_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:buscar/common_widgets/flat_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class FindRideScreen extends StatelessWidget {
@@ -75,18 +76,35 @@ class GoogleMapWidget extends StatefulWidget {
 }
 
 class _GoogleMapWidgetState extends State<GoogleMapWidget> {
+  late String _darkMapStyle =
+      rootBundle.loadString('assets\json\map_dark_theme.json').toString();
+  late String _lightMapStyle =
+      rootBundle.loadString('assets\json\map_dark_theme.json').toString();
+  late String _currentMapStyle;
+
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
   static const CameraPosition initialPos = CameraPosition(
       target: LatLng(-23.316321971739562, -51.17272870369577),
-      zoom: 19.151926040649414);
+      zoom: 15.151926040649414);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    (Theme.of(context).brightness == Brightness.dark)
+        ? _currentMapStyle = _darkMapStyle
+        : _currentMapStyle = _lightMapStyle;
+
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: initialPos,
       onMapCreated: (GoogleMapController controller) {
+        controller.setMapStyle(_currentMapStyle);
         _controller.complete(controller);
       },
     );
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Istok Web',
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 219, 238, 253),
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
         ),
       ),
     );
